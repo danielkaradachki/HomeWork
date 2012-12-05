82172b1bdb096ba6e4d2d543ee637956f43c2c6a
Daniel Karadachki <karada@abv.bg>
2012-12-06 00:39:50 +0200

    Persist TreeViewState example

---
+++ PersistStateKendoTreeView/PersistStateKendoTreeView/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PersistStateKendoTreeView.Models;
+
+namespace PersistStateKendoTreeView.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            
+            ViewData["Customers"] = GetData();
+
+            ViewData["ExpandedNodes"] = Request.Cookies["ExpandedNodes"] != null ?
+                                        HttpUtility.UrlDecode(Request.Cookies["ExpandedNodes"].Value).Split(';')
+                                      : new string[] { };
+
+            return View();
+        }
+
+        private IEnumerable<Customer> GetData()
+        {
+            NorthwindEntities context = new NorthwindEntities();
+            return from c in context.Customers.Take(10) select c;
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
