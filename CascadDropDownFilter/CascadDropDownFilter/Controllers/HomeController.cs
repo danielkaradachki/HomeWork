92cfc074ebdae902e15154c71a5af5ad179e0afe
Atanas Korchev <akorchev@gmail.com>
2013-11-04 15:09:10 +0200

    Cascading filter sample

---
+++ CascadDropDownFilter/CascadDropDownFilter/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+using CascadDropDownFilter.Models;
+
+namespace CascadDropDownFilter.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to ASP.NET MVC!";
+
+            return View();
+        }
+
+        public ActionResult Read([DataSourceRequest] DataSourceRequest request)
+        {
+            return Json(new NorthwindEntities().Customers.ToDataSourceResult(request));
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+		public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
